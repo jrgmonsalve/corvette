fac3abf85ac6161bf98cd915f4ea1f617e23d972
Johan Garcia <johangarcia@outlook.com>
2024-02-12 20:59:27 -0500

    feat: generate cpu profile

--- src/main.go
+++ src/main.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"runtime/pprof"
+	"syscall"
 )
 
 func main() {
-
-	////Prceso de rendimiento de la aplicación/////////////
-	cpu, err := os.Create("cpu.prof")
+	// Crear archivo de perfil de CPU
+	f, err := os.Create("cpu.prof")
 	if err != nil {
 		log.Fatal(err)
 	}
-	pprof.StartCPUProfile(cpu)
-	defer pprof.StopCPUProfile()
-	////////Fin prceso de rendimiento de la aplicación/////////
+	defer f.Close()
+
+	// Iniciar perfilado de CPU
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("Error starting CPU profile: ", err)
+	}
+
+	// Preparar manejo de señal de interrupción (Ctrl+C)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// Usar una goroutine para manejar la señal
+	go func() {
+		<-c
+		fmt.Println("\nCtrl+C pressed. Stopping CPU profile and exiting...")
+		pprof.StopCPUProfile() // Detener el perfilado de CPU
+		os.Exit(0)
+	}()
 
 	LoadEnvFile()
 
@@ -35,4 +51,5 @@ func main() {
 		}
 	}
 
+	pprof.StopCPUProfile()
 }
